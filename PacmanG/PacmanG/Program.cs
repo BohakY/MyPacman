45fd6fee265cbe9cc8d71a8c934e7cb5762eddfc
Yaroslav Bohak <yaroslavbohak@gmail.com>
2018-06-06 08:55:03 +0300

    Add project

---
+++ PacmanG/PacmanG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rwet");
+            Console.ReadKey();
+        }
+    }
+}
